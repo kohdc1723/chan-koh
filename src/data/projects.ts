9f8dca3cc5855efc24335b9657a96ab11b5b8dd5
kohdc <kohdc1723@gmail.com>
2024-03-12 18:00:38 -0700

    add projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,6 +5,9 @@ import { RiExternalLinkLine } from "react-icons/ri";
 
 import TrelloClone from "@/../public/trello-clone.png";
 import ChanKoh from "@/../public/chan-koh.png";
+import CookingHome from "@/../public/cooking-home.png";
+import LoveYourPlanet from "@/../public/love-your-planet.jpg";
+import DeveloperJourneyMap from "@/../public/developer-journey-map.png";
 
 type LinkType = {
     display: string;
@@ -60,6 +63,61 @@ const projects: ProjectType[] = [
                 display: "github/trello-clone",
                 url: "https://github.com/kohdc1723/trello-clone",
                 icon: SiGithub
+            }
+        ]
+    },
+    {
+        title: "Cooking Home",
+        description: "A mobile-first web application that provides a recipe search feature and offers recipe recommendations",
+        startDate: "Oct 2023",
+        endDate: "Dec 2023",
+        techStack: ["JavaScript", "React", "MUI", "Redux Toolkit", "RTK Query", "Edamam API", "Node.js", "Express", "MongoDB", "Mongoose", "CI/CD", "AWS", "Github Actions"],
+        thumbnail: CookingHome,
+        links: [
+            {
+                display: "https://cooking-home.kro.kr",
+                url: "https://cooking-home.kro.kr",
+                icon: RiExternalLinkLine
+            },
+            {
+                display: "github/cooking-home-frontend",
+                url: "https://github.com/kohdc1723/cooking-home-frontend",
+                icon: SiGithub
+            },
+            {
+                display: "github/cooking-home-backend",
+                url: "https://github.com/kohdc1723/cooking-home-backend",
+                icon: SiGithub
+            }
+        ]
+    },
+    {
+        title: "Love Your Planet Mobile App",
+        description: "A mobile application, an easy-to-use social media for sustainability",
+        startDate: "Apr 2023",
+        endDate: "May 2023",
+        techStack: ["JavaScript", "React Native", "Redux", "Node.js", "Express", "MongoDB", "Firebase", "Heroku"],
+        thumbnail: LoveYourPlanet,
+        links: [
+            {
+                display: "notion/love-your-planet-mobile-app",
+                url: "https://stump-clavicle-74d.notion.site/Love-Your-Planet-Mobile-App-b6db8484bdb84548991dc0b169ebf4a7",
+                icon: SiNotion
+            }
+        ]
+    },
+    {
+        title: "Developer Journey Map",
+        description: "A full-stack web application, a visualization tool identifies the path the developer experiences when learning or adopting new technologies",
+        startDate: "Jan 2023",
+        endDate: "Apr 2023",
+        techStack: ["JavaScript", "React", "Tailwind CSS", "Node.js", "Express", "MongoDB", "Mongoose", "Firebase", "Google Cloud"],
+        thumbnail: DeveloperJourneyMap,
+        links: [
+            {
+                display: "notion/developer-journey-map",
+                url: "https://stump-clavicle-74d.notion.site/Developer-Journey-Map-1811046303934666abf5cff0bd9f84e5",
+                icon: SiNotion
             }
         ]
     }
